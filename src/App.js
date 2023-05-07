5b17a9e924b15af99c693e0f152c5688f3228fcb
hmphne <3rdoperson@gmail.com>
2023-05-07 16:45:16 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { DatePicker } from 'jalaali-react-date-picker';
+import "jalaali-react-date-picker/lib/styles/index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <DatePicker />
+    </div>
+  );
+}
+
+export default App;
